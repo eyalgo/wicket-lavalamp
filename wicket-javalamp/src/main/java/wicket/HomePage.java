df611eb7e3bc9d553960c6d289ec3b0320b73995
egolan74 <egolan74@e935fc0c-881f-11de-ae39-2786408ab335>
2009-08-13 22:42:33 +0000

    initial add of the panel for the menu

--- wicket-javalamp/src/main/java/wicket/HomePage.java
+++ wicket-javalamp/src/main/java/wicket/HomePage.java
@@ -1,36 +1,25 @@
 package wicket;
 
-
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.CSSPackageResource;
-import org.apache.wicket.markup.html.JavascriptPackageResource;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
-import wicket.main.JQueryLavaLampBehavior;
+import wicket.lavalamp.LavaLampMenuPanel;
 
 /**
  * Homepage
  */
 public class HomePage extends WebPage {
-
-	private static final long serialVersionUID = 1L;
-
-	// TODO Add any page properties or variables here
-
     /**
-	 * Constructor that is invoked when page is invoked without a session.
-	 * 
-	 * @param parameters
-	 *            Page parameters
-	 */
+     * Constructor that is invoked when page is invoked without a session.
+     * 
+     * @param parameters
+     *            Page parameters
+     */
     public HomePage(final PageParameters parameters) {
-    	add(new JQueryLavaLampBehavior());
-    	add(CSSPackageResource.getHeaderContribution(JQueryLavaLampBehavior.class, "lavalamp.css"));
-    	add(JavascriptPackageResource.getHeaderContribution(HomePage.class, "headlamp.js"));
+        add(new LavaLampMenuPanel("lavalampMenu"));
         // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
-
-        // TODO Add your page's components here
+        add(new Label("message",
+                "If you see this message wicket is properly configured and running"));
     }
 }
