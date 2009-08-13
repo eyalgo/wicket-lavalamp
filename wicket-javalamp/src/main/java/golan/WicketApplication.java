377168867465a7a2ee3d85d886a6f5a2ca70b824
egolan74 <egolan74@e935fc0c-881f-11de-ae39-2786408ab335>
2009-08-13 15:54:38 +0000

    Initial add

---
+++ wicket-javalamp/src/main/java/golan/WicketApplication.java
@@ -0,0 +1,26 @@
+package golan;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start
+ * class.
+ * 
+ * @see golan.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication {
+	/**
+	 * Constructor
+	 */
+	public WicketApplication() {
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage() {
+		return HomePage.class;
+	}
+
+}
