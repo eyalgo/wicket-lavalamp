eab9d461bb095faffa604995f067e3c7029a9149
egolan74 <egolan74@e935fc0c-881f-11de-ae39-2786408ab335>
2009-08-14 00:59:29 +0000

    the menu panel is abstract. it enforces the user to supply the CSS resource

--- trunk/wicket-javalamp/src/main/java/wicket/HomePage.java
+++ trunk/wicket-javalamp/src/main/java/wicket/HomePage.java
@@ -1,8 +1,10 @@
 package wicket;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 
 import wicket.lavalamp.LavaLampMenuPanel;
 
@@ -17,7 +19,14 @@ public class HomePage extends WebPage {
      *            Page parameters
      */
     public HomePage(final PageParameters parameters) {
-        add(new LavaLampMenuPanel("lavalampMenu"));
+        add(new LavaLampMenuPanel("lavalampMenu") {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected ResourceReference getCssResourceReference() {
+                return new CompressedResourceReference(HomePage.class, "lavalamp.css");
+            }
+        });
         // Add the simplest type of label
         add(new Label("message",
                 "If you see this message wicket is properly configured and running"));
