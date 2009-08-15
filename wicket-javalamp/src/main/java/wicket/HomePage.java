ea97a9b5917907f0aaf4b8774906abe70dc4834f
egolan74 <egolan74@e935fc0c-881f-11de-ae39-2786408ab335>
2009-08-15 10:34:30 +0000

    using MenuItem

--- wicket-javalamp/src/main/java/wicket/HomePage.java
+++ wicket-javalamp/src/main/java/wicket/HomePage.java
@@ -1,17 +1,26 @@
 package wicket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 
 import wicket.lavalamp.LavaLampMenuPanel;
+import wicket.lavalamp.MenuItem;
 
 /**
  * Homepage
  */
 public class HomePage extends WebPage {
+    public final String CSS_CLASS_NAME = "lavaLamp";
+
     /**
      * Constructor that is invoked when page is invoked without a session.
      * 
@@ -19,12 +28,62 @@ public class HomePage extends WebPage {
      *            Page parameters
      */
     public HomePage(final PageParameters parameters) {
-        add(new LavaLampMenuPanel("lavalampMenu") {
+        IModel<List<MenuItem>> model = new AbstractReadOnlyModel<List<MenuItem>>() {
+
+            @Override
+            public List<MenuItem> getObject() {
+                final List<MenuItem> result = new ArrayList<MenuItem>();
+                result.add(new MenuItem(new Link<Object>(MenuItem.LINK_ID) {
+
+                    @Override
+                    public void onClick() {
+                    // TODO Auto-generated method stub
+
+                    }
+
+                }, new Label(MenuItem.CAPTION_ID, "One")));
+                result.add(new MenuItem(new Link<Object>(MenuItem.LINK_ID) {
+
+                    @Override
+                    public void onClick() {
+                    // TODO Auto-generated method stub
+
+                    }
+
+                }, new Label(MenuItem.CAPTION_ID, "Two")));
+                result.add(new MenuItem(new Link<Object>(MenuItem.LINK_ID) {
+
+                    @Override
+                    public void onClick() {
+                    // TODO Auto-generated method stub
+
+                    }
+
+                }, new Label(MenuItem.CAPTION_ID, "Three")));
+                result.add(new MenuItem(new Link<Object>(MenuItem.LINK_ID) {
+
+                    @Override
+                    public void onClick() {
+                    // TODO Auto-generated method stub
+
+                    }
+
+                }, new Label(MenuItem.CAPTION_ID, "Four")));
+                return result;
+            }
+        };
+
+        add(new LavaLampMenuPanel("lavalampMenu", model) {
             private static final long serialVersionUID = 1L;
 
             @Override
             protected ResourceReference getCssResourceReference() {
                 return new CompressedResourceReference(HomePage.class, "lavalamp.css");
+            }
+
+            @Override
+            protected String getCssClassName() {
+                return CSS_CLASS_NAME;
             }
         });
         // Add the simplest type of label
